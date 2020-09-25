3259562a36c14f3d84f16ce85a5ab3d3579fc0de
Felix murimi <61313608+Felix280murimi@users.noreply.github.com>
2020-09-25 13:24:37 +0300

    Add project files.

---
+++ do while , until/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnClickToShow_Click(sender As Object, e As EventArgs) Handles btnClickToShow.Click
+        Dim MaraNgapi As Integer
+        Dim i As Integer, jina As String
+        i = 1
+        jina = InputBox("Enter your name please")
+        MaraNgapi = InputBox(jina & " How many times do you want the message to be displayed")
+        Do Until (i > MaraNgapi)
+            MessageBox.Show(i & jina & " Smile Rejoice, God loves you and Fay loves you")
+            i = i + 1
+        Loop
+        MessageBox.Show(jina & "Praise and Glorify the Almighty God")
+    End Sub
+End Class
